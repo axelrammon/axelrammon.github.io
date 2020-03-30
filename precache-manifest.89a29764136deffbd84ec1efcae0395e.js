5e267faadceb603791d0aba89f4839dd41c85c1a
Axel Rammon <axelrammon48@gmail.com>
2020-03-30 13:14:59 -0300

    Add files via upload

---
+++ precache-manifest.89a29764136deffbd84ec1efcae0395e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c650430eccd67b12c3b6047582b8d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35288306c8c4c90e3cc6",
+    "url": "/static/css/main.0ffc18d6.chunk.css"
+  },
+  {
+    "revision": "697202948588215c7c96",
+    "url": "/static/js/2.30df6175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30df6175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35288306c8c4c90e3cc6",
+    "url": "/static/js/main.6e4bf42c.chunk.js"
+  },
+  {
+    "revision": "b79d706e2a163d6f6157",
+    "url": "/static/js/runtime-main.b3679475.js"
+  },
+  {
+    "revision": "d635a7ea9a42cae228c47aed8b820932",
+    "url": "/static/media/me.d635a7ea.jpg"
+  }
+]);
