6991345620a11a396028bbc7c044bf2e618989b8
axelrammon <axelrammon48@gmail.com>
2022-06-02 12:23:38 -0300

    update portifolio

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,9 @@
 let botao = document.querySelector('#btn-dark');
 let header = document.querySelector('#cabecalho');
-let socials = document.querySelector('#socials');
 let conteudo = document.querySelector('#conteudo-principal');
-let link_nav = document.querySelector('a');
+let link_nav1 = document.querySelector('#nav1');
+let link_nav2 = document.querySelector('#nav2');
+let link_nav3 = document.querySelector('#nav3');
 
 let darkMode = false;
 
@@ -10,20 +11,21 @@ botao.addEventListener('click', () => {
   if (darkMode === false) {
     botao.classList = 'bg-dark';
     header.classList = 'bg-dark';
-    socials.classList = 'bg-dark';
     conteudo.classList = 'bg-dark';
-    link_nav.classList = 'bg-dark';
+    link_nav1.classList = 'bg-dark';
+    link_nav2.classList = 'bg-dark';
+    link_nav3.classList = 'bg-dark';
 
     darkMode = true;
   } else {
     botao.classList = 'bg-light';
     header.classList = 'bg-light';
-    socials.classList = 'bg-light';
     conteudo.classList = 'bg-light';
-    link_nav.classList = 'bg-light';
+    link_nav1.classList = 'bg-light';
+    link_nav2.classList = 'bg-light';
+    link_nav3.classList = 'bg-light';
 
     darkMode = false;
   }
 });
 
-console.log(botao)
