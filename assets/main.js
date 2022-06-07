13dd181c650b1ca640d6309d791854d4f8c7129f
axelrammon <axelrammon48@gmail.com>
2022-06-06 22:15:36 -0300

    update

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ let header = document.querySelector('#cabecalho');
 let conteudo = document.querySelector('#conteudo-principal');
 let nav = document.querySelectorAll('.nav-menu');
 let socials = document.querySelectorAll('.bg-socials');
+let iconDark = document.querySelector('.material-icons');
 let darkMode = false;
 
 botao.addEventListener('click', () => {
@@ -12,6 +13,8 @@ botao.addEventListener('click', () => {
     conteudo.classList = 'bg-dark';
     nav.forEach(item => {item.classList = "bg-dark"});
     socials.forEach(item => {item.classList = "bg-dark"});
+    iconDark.innerText = 'light_mode';
+    iconDark.style.color = 'yellow'
 
     darkMode = true;
   } else {
@@ -20,6 +23,8 @@ botao.addEventListener('click', () => {
     conteudo.classList = 'bg-light';
     nav.forEach(item => {item.classList = "bg-light"});
     socials.forEach(item => {item.classList = "bg-light"});
+    iconDark.innerText = 'dark_mode';
+    iconDark.style.color = '#024a56'
 
     darkMode = false;
   }
