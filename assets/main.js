463869cc42991649d26c3850d8cda83db2355f24
axelrammon <axelrammon48@gmail.com>
2022-06-08 10:51:00 -0300

    add projects and dark mode complete

--- assets/main.js
+++ assets/main.js
@@ -5,27 +5,35 @@ let nav = document.querySelectorAll('.nav-menu');
 let socials = document.querySelectorAll('.bg-socials');
 let iconDark = document.querySelector('.material-icons');
 let projetos = document.querySelector('#projetos');
+let experiencias = document.querySelector('#experiencias');
+let rodape = document.querySelector('#rodape');
+let cardProjetos = document.querySelectorAll('.projetos-content');
+let ancoraCardProjetos = document.querySelectorAll('.a-projetos-content');
 let darkMode = false;
 
+let componentes = [botao, header, conteudo, experiencias];
+let componentes2 = [projetos, rodape];
+
+console.log(cardProjetos)
 botao.addEventListener('click', () => {
   if (darkMode === false) {
-    botao.classList = 'bg-dark';
-    header.classList = 'bg-dark';
-    conteudo.classList = 'bg-dark';
-    nav.forEach(item => {item.classList = "bg-dark"});
-    socials.forEach(item => {item.classList = "bg-dark"});
-    projetos.classList = 'bg-dark2'
+    componentes.forEach(item => {item.classList = 'bg-dark'});
+    nav.forEach(item => {item.classList = 'bg-dark'});
+    socials.forEach(item => {item.classList = 'bg-dark'});
+    componentes2.forEach(item => {item.classList = 'bg-dark2'});
+    cardProjetos.forEach(item => {item.classList = 'projetos-content bg-dark'});
+    ancoraCardProjetos.forEach(item => {item.classList = 'bg-dark'});
     iconDark.innerText = 'light_mode';
     iconDark.style.color = 'yellow'
 
     darkMode = true;
   } else {
-    botao.classList = 'bg-light';
-    header.classList = 'bg-light';
-    conteudo.classList = 'bg-light';
+    componentes.forEach(item => {item.classList = 'bg-light'});
     nav.forEach(item => {item.classList = "bg-light"});
     socials.forEach(item => {item.classList = "bg-light"});
-    projetos.classList = 'bg-light2'
+    componentes2.forEach(item => {item.classList = 'bg-light2'});
+    cardProjetos.forEach(item => {item.classList = 'projetos-content bg-light'});
+    ancoraCardProjetos.forEach(item => {item.classList = 'bg-light'});
     iconDark.innerText = 'dark_mode';
     iconDark.style.color = '#024a56'
 
